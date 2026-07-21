bb2357e588a6c7ff134fd095974ada449c716a66
Sumit <sumitkumarjha770@gmail.com>
2026-07-21 11:53:31 +0530

    spring bean xml type creation

---
+++ Spring-Bean-XML-Type/src/main/java/com/kodewala/student/Student.java
@@ -0,0 +1,55 @@
+package com.kodewala.student;
+
+public class Student {
+	
+	
+	private String name;
+	private String id;
+	private String course;
+	
+	
+	public void displayStudentInfo() {
+		
+		System.out.println("this will work and display the student info ");
+		
+		System.out.println("[name=" + name + ", id=" + id + ", course=" + course + "]");
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", course=" + course + "]";
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getCourse() {
+		return course;
+	}
+
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+}
---
+++ Spring-Bean-XML-Type/src/main/java/com/kodewala/user/App.java
@@ -0,0 +1,25 @@
+package com.kodewala.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.kodewala.student.Student;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       ApplicationContext context = new ClassPathXmlApplicationContext("com/kodewala/user/beans.xml");
+       
+       
+       Student student =(Student)context.getBean("obj");
+       
+       
+      // student.displayStudentInfo();
+       
+       student.setCourse("JAVA");
+       student.setId("A123");
+       student.setName("Sumit");
+       student.displayStudentInfo();
+    }
+}
