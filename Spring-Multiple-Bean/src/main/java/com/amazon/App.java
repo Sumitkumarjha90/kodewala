74976f5aad98658c1df29c445cafd4f7f312d9fb
Sumit <sumitkumarjha770@gmail.com>
2026-07-31 11:40:56 +0530

    Spring multiple bean Qualifiers and primary added

---
+++ Spring-Multiple-Bean/src/main/java/com/amazon/App.java
@@ -0,0 +1,25 @@
+package com.amazon;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.amazon.config.SpringConfig;
+
+import com.amazon.payment.PaymentService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        
+    	PaymentService pay = context.getBean(PaymentService.class);
+    	
+    	pay.doPayment();
+    	
+    	
+    	
+    	
+    }
+}
---
+++ Spring-Multiple-Bean/src/main/java/com/amazon/payment/Payment.java
@@ -0,0 +1,20 @@
+package com.amazon.payment;
+
+public class Payment {
+	
+	private String refId;
+	
+	
+	
+	public Payment(String refId) {
+		super();
+		this.refId = refId;
+	}
+
+
+
+	public void pay() {
+		System.out.println("Payment.pay().........."+refId);
+	}
+
+}
